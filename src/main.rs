63f5718f789b7b1890c0472fc8dc8e4f364d0654
MashPlant <740678788@qq.com>
2020-05-01 18:26:42 +0800

    support remove reply

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 #[macro_use]
 extern crate log;
 
-use fuse::{Filesystem, Request, ReplyEntry, ReplyAttr, ReplyDirectory, FileType::*, FileAttr, ReplyData, ReplyWrite, ReplyOpen};
+use fuse::{Filesystem, Request, ReplyEntry, ReplyAttr, ReplyDirectory, FileType::*, FileAttr, ReplyData, ReplyWrite, ReplyOpen, ReplyEmpty};
 use libc::{ENOENT, EIO, EPERM};
 use tokio::runtime::Runtime;
 use futures::future::{try_join_all, try_join4};
@@ -16,6 +16,10 @@ use InoInfo::*;
 // there is no need to use BTreeMap / HashMap / IndexMap for performance
 // this simple solution keeps insertion order, and is quite fast when dealing with small amount of data
 type Map = Vec<(String, u64)>;
+
+fn do_lookup<S: Borrow<str>>(m: impl IntoIterator<Item=impl Borrow<(S, u64)>>, k: &str) -> Option<u64> {
+  m.into_iter().find(|x| x.borrow().0.borrow() == k).map(|x| x.borrow().1)
+}
 
 // all Map's value are pointer to other ino
 enum InoInfo {
@@ -72,7 +76,8 @@ impl Content {
 }
 
 enum RefreshInfo {
-  Homework { course: String, homework: String }
+  Homework { course: String, homework: String },
+  Discussion, // no extra data needs to be kept
 }
 
 // FileSystem's ino id starts from 1, fill inos[0] with Root, though it won't be accessed
@@ -152,27 +157,31 @@ macro_rules! try_push {
 
 fn bool2str(b: bool) -> &'static str { if b { "是" } else { "否" } }
 
-fn homework_content(h: Homework, mut i: u64, client: Arc<LearnHelper>) -> (Vec<(Cow<'static, str>, u64)>, Vec<Content>) {
+// if content_only == false, will push extra 2 items to `m`, respectively `SubmitHomework` and `Refresh`
+fn homework_content(h: Homework, mut i: u64, client: &Arc<LearnHelper>, content_only: bool) -> (Vec<(Cow<'static, str>, u64)>, Vec<Content>) {
   let HomeworkDetail { description, attachment_name_url, submit_attachment_name_url, grade_attachment_name_url } = h.detail;
   let (mut m, mut c) = (Vec::new(), Vec::new());
+  if !content_only {
+    // they should added in the caller side of `homework_content`
+    m.push(("提交作业".into(), inc!(i))); // it maps to SubmitHomework
+    m.push(("刷新".into(), inc!(i))); // it maps to Refresh
+  }
   push!(m, c, i, "描述" => description, "发布时间" => h.assign_time.to_string(), "截止时间" => h.deadline.to_string());
   try_push!(m, c, i, "提交时间" => h.submit_time.map(|x| x.to_string()), "提交内容" => h.submit_content,
     "成绩" => h.grade.map(|x| x.to_string()), "批阅时间" => h.grade_time.map(|x| x.to_string()),
     "批阅老师" => h.grader_name, "评语" => h.grade_content);
   if let Some((name, url)) = attachment_name_url {
     m.push((format!("附件：{}", name).into(), inc!(i)));
-    c.push(Content::Url(url, Arc::clone(&client)));
+    c.push(Content::Url(url, Arc::clone(client)));
   }
   if let Some((name, url)) = submit_attachment_name_url {
     m.push((format!("提交附件：{}", name).into(), inc!(i)));
-    c.push(Content::Url(url, Arc::clone(&client)));
+    c.push(Content::Url(url, Arc::clone(client)));
   }
   if let Some((name, url)) = grade_attachment_name_url {
-    m.push((format!("评语附件：{}", name).into(), inc!(i)));
-    c.push(Content::Url(url, Arc::clone(&client)));
+    m.push((format!("评语附件：{}", name).into(), i));
+    c.push(Content::Url(url, Arc::clone(client)));
   }
-  m.push(("提交作业".into(), inc!(i))); // it will map to SubmitHomework, added in the caller side of `homework_content`
-  m.push(("刷新".into(), i)); // it will map to Refresh, added in the caller side of `homework_content`
   (m, c)
 }
 
@@ -197,12 +206,12 @@ fn file_content(f: File, mut i: u64, client: Arc<LearnHelper>) -> (Vec<(Cow<'sta
 }
 
 macro_rules! unwrap {
-  ($res: expr, $reply: expr, $err: expr) => {
+  ($res: expr, $reply: expr) => {
     match $res {
       Ok(x) => x,
       Err(e) => {
         warn!("line {}: {:?}", line!(), e);
-        return $reply.error($err);
+        return $reply.error(EIO);
       }
     }
   };
@@ -216,16 +225,16 @@ impl LearnFS {
     let mut new_ino = self.inos.len() as u64;
     match &mut self.inos[ino as usize] {
       Discussion { course_discussion, board, client, replies } => {
-        if replies.is_empty() {
+        if replies.len() == 1 { // assume len >= 1
           let (course, discussion) = (&course_discussion.0, &course_discussion.1);
           let replies1 = match self.runtime.block_on(client.discussion_replies(course, discussion, board)) {
             Ok(x) => x,
             Err(e) => return (warn!("line {}: {:?}", line!(), e), false).1,
           };
           for (i, r) in replies1.iter().enumerate() {
-            replies.push((format!("{}楼-{}-{}", i + 1, r.author, r.publish_time), inc!(new_ino)));
+            replies.push((format!("{}楼-{}-{}", i, r.author, r.publish_time), inc!(new_ino)));
             for (j, r) in r.replies.iter().enumerate() {
-              replies.push((format!("{}楼-回复{}-{}-{}", i + 1, j + 1, r.author, r.publish_time), inc!(new_ino)));
+              replies.push((format!("{}楼-回复{}-{}-{}", i, j, r.author, r.publish_time), inc!(new_ino)));
             }
           }
           let (course_discussion, client) = (Arc::clone(course_discussion), Arc::clone(client));
@@ -255,9 +264,6 @@ impl LearnFS {
 impl Filesystem for LearnFS {
   fn lookup(&mut self, _req: &Request, parent: u64, name: &OsStr, reply: ReplyEntry) {
     info!("lookup parent={} name={:?}", parent, name);
-    fn do_lookup<S: Borrow<str>>(m: impl IntoIterator<Item=impl Borrow<(S, u64)>>, k: &str) -> Option<u64> {
-      m.into_iter().find(|x| x.borrow().0.borrow() == k).map(|x| x.borrow().1)
-    }
     let ref name = name.to_string_lossy();
     fn reply_map<S: Borrow<str>>(m: impl IntoIterator<Item=impl Borrow<(S, u64)>>, name: &str, reply: ReplyEntry) {
       if let Some(ino) = do_lookup(m, &name) {
@@ -297,7 +303,7 @@ impl Filesystem for LearnFS {
           let (course, client) = (Arc::clone(id), Arc::clone(client));
           let (hs, ns, fs, ds) = unwrap!(self.runtime.block_on(try_join4(
             client.homework_list(&course), client.notification_list(&course),
-            client.file_list(&course), client.discussion_list(&course))), reply, EIO);
+            client.file_list(&course), client.discussion_list(&course))), reply);
           *fetched = true;
           macro_rules! handle_items {
             ($items: expr, $content_fn: expr, $offset: expr) => {
@@ -316,11 +322,11 @@ impl Filesystem for LearnFS {
             let new_ino = self.inos.len() as u64;
             let (name, sh, course, homework) =
               (get(&mut h.title), get(&mut h.student_homework_id), get(&mut h.course_id), get(&mut h.id));
-            let (m, cs) = homework_content(h, new_ino + 1, Arc::clone(&client));
+            let (m, cs) = homework_content(h, new_ino + 1, &client, false);
             self.inos.push(Item(m));
-            for c in cs { self.inos.push(Content(c)); }
             self.inos.push(SubmitHomework { student_homework: Arc::new(sh), client: Arc::clone(&client) });
             self.inos.push(Refresh { parent: new_ino, client: Arc::clone(&client), info: RefreshInfo::Homework { course, homework } });
+            for c in cs { self.inos.push(Content(c)); }
             match &mut self.inos[parent as usize + 1] { ItemList(m) => m.push((name, new_ino)), _ => unreachable!() }
           }
           handle_items!(ns, notification_content, 2);
@@ -331,8 +337,9 @@ impl Filesystem for LearnFS {
               course_discussion: Arc::new((Arc::clone(&course), d.id)),
               board: d.board_id,
               client: Arc::clone(&client),
-              replies: Vec::new(),
+              replies: vec![("刷新".into(), new_ino + 1)],
             });
+            self.inos.push(Refresh { parent: new_ino, client: Arc::clone(&client), info: RefreshInfo::Discussion });
             match &mut self.inos[parent as usize + 4] { ItemList(m) => m.push((d.title, new_ino)), _ => unreachable!() }
           }
         }
@@ -359,7 +366,7 @@ impl Filesystem for LearnFS {
     match &mut self.inos[parent as usize] {
       Root { users } => {
         // we don't need to worry about duplication here, because a prior `lookup` call will prevent it
-        let password = unwrap!(get_password(req.pid()), reply, EIO);
+        let password = unwrap!(get_password(req.pid()), reply);
         let tmp = self.runtime.block_on(async {
           let cl = LearnHelper::login(&name, &password).await?;
           let ss = cl.semester_id_list().await?;
@@ -367,7 +374,7 @@ impl Filesystem for LearnFS {
           let css = try_join_all(ss.iter().map(async move |s| cl1.course_list(s).await)).await?;
           Ok::<_, Error>((Arc::new(cl), ss, css))
         });
-        let (cl, ss, css) = unwrap!(tmp, reply, EIO);
+        let (cl, ss, css) = unwrap!(tmp, reply);
         let ss1 = ss.iter().map(|s| {
           let (l, r) = s.split_at(s.len() - 1);
           l.to_owned() + match r { "1" => "秋", "2" => "春", "3" => "夏", _ => panic!("invalid semester type"), }
@@ -393,12 +400,33 @@ impl Filesystem for LearnFS {
     }
   }
 
+  fn unlink(&mut self, _req: &Request, parent: u64, name: &OsStr, reply: ReplyEmpty) {
+    info!("unlink parent={} name={:?}", parent, name);
+    let name = name.to_string_lossy();
+    match &mut self.inos[parent as usize] {
+      Discussion { replies, .. } => {
+        if let Some(idx) = do_lookup(replies, &name) {
+          match &self.inos[idx as usize] {
+            DiscussionReply { course_discussion, id, client, .. } => {
+              let id = if let Some(x) = id.as_ref() { x } else { return reply.error(EPERM); };
+              let _ = unwrap!(self.runtime.block_on(client.delete_discussion_reply(&course_discussion.0, id)), reply);
+              reply.ok();
+            }
+            Refresh { .. } => reply.error(EPERM),
+            _ => unreachable!(),
+          }
+        } else { reply.error(ENOENT); }
+      }
+      _ => reply.error(EPERM),
+    }
+  }
+
   fn open(&mut self, _req: &Request, ino: u64, _flags: u32, reply: ReplyOpen) {
     info!("open ino={}", ino);
     if let Content(Content::Url(url, client)) = &mut self.inos[ino as usize] {
       self.inos[ino as usize] = Content(Content::Data(unwrap!(self.runtime.block_on(async {
         client.0.get(url.as_str()).send().await?.bytes().await
-      }), reply, EIO)));
+      }), reply)));
     }
     // this is the default implementation of `FileSystem`, returning such data makes the `open` operation useless
     reply.opened(0, 0);
@@ -413,7 +441,7 @@ impl Filesystem for LearnFS {
     match &self.inos[ino as usize] {
       Content(c) => reply_bytes(c.bytes(), reply),
       DiscussionReply { content, .. } => reply_bytes(content.as_bytes(), reply),
-      SubmitHomework { .. } => reply.data(&[]),
+      SubmitHomework { .. } | Refresh { .. } => reply.data(&[]),
       _ => reply.error(EPERM),
     }
   }
@@ -448,27 +476,31 @@ impl Filesystem for LearnFS {
         let parent = *parent;
         match info {
           RefreshInfo::Homework { course, homework } => {
-            let hs = unwrap!(self.runtime.block_on(client.homework_list(course)), reply, EIO);
+            let hs = unwrap!(self.runtime.block_on(client.homework_list(course)), reply);
             if let Some(h) = hs.into_iter().find(|h| &h.id == homework) {
               let new_ino = self.inos.len() as u64;
-              let (m, cs) = homework_content(h, new_ino, Arc::clone(client));
+              let (mut m, cs) = homework_content(h, new_ino, client, true);
               for c in cs { self.inos.push(Content(c)); }
               let m1 = match &mut self.inos[parent as usize] { Item(m) => m, _ => unreachable!() };
-              let (submit_homework, refresh) = {
-                let mut it = m1.drain(m1.len() - 2..);
-                (it.next().unwrap(), it.next().unwrap())
-              };
-              // for convenience, except for the last two files, the space occupied by other files has not been recycled
+              // for convenience, except for the first two files, the space occupied by other files has not been recycled
               // this does cause a waste of space, and it would be better if we had a global garbage collector
               // now we can pretend that it exists, so there is no need to implement refreshing very carefully
-              *m1 = m;
-              (m1.pop(), m1.pop(), m1.push(submit_homework), m1.push(refresh));
+              m1.truncate(2);
+              m1.append(&mut m);
+            }
+          }
+          RefreshInfo::Discussion => {
+            match &mut self.inos[parent as usize] {
+              // just truncate its length to 1, and subsequent operations will refill the data
+              Discussion { replies, .. } => replies.truncate(1),
+              _ => unreachable!(),
             }
           }
         }
         reply.written(data.len() as u32);
       }
       DiscussionReply { course_discussion, id, client, .. } => {
+        reply.written(data.len() as u32);
         let (pid, data) = (req.pid(), data.to_vec());
         let (course_discussion, id, client) = (Arc::clone(course_discussion), Arc::clone(id), Arc::clone(&client));
         self.runtime.spawn(async move {
@@ -516,7 +548,7 @@ impl Filesystem for LearnFS {
 fn main() {
   env_logger::init();
   let mountpoint = if let Some(x) = std::env::args_os().nth(1) { x } else {
-    eprintln!("使用方法： <程序> <挂载点>");
+    eprintln!("使用方法：<程序> <挂载点>");
     std::process::exit(1);
   };
   fuse::mount(LearnFS::new(), mountpoint, &[]).unwrap();
